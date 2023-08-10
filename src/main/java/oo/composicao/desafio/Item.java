c5a62f80eb1669792d0312da7b1b7454a731637d
Alexandre Gomes <algomjo@live.com>
2023-08-10 16:37:23 -0300

    10/08/2023

---
+++ src/main/java/oo/composicao/desafio/Item.java
@@ -0,0 +1,16 @@
+package oo.composicao.desafio;
+
+import java.util.List;
+
+
+public class Item {
+    final Produto produto;
+    final int quantidade;
+
+    public Item(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+    
+    
+}
