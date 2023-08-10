c6b2c09fe8b89de82903bb5143b536878caaea60
Alexandre Gomes <algomjo@live.com>
2023-08-09 23:05:46 -0300

    09/08/2023

--- src/main/java/oo/composicao/Compra.java
+++ src/main/java/oo/composicao/Compra.java
@@ -7,4 +7,12 @@ public class Compra {
     
     String cliente;
     ArrayList<Item> itens = new ArrayList<>();
+    
+    double obterValorTotal(){
+        double total = 0;
+        for (Item item : itens) {
+            total += item.quantidade * item.preco;
+        }
+        return total;
+    }
 }
